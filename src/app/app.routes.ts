2ecdb6e0736fafa590c79d71cecf8cfe041b04db
saili <saili.hamza10@gmail.com>
2024-03-03 11:33:17 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component:Test1Component, title:'home page',
+    },
+    {
+        path:'test2', component:Test2Component, title:'test2',
+    
+    }
+]; 
+
+export default routes;
